56c6a6062f668a6afb2079ed703d9186f930789e
alea12 <mail@alea12.net>
2017-03-08 11:51:35 +0900

    add test for ExChatwork.get_rooms

--- test/exchatwork_test.exs
+++ test/exchatwork_test.exs
@@ -1,8 +1,22 @@
 defmodule ExChatworkTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
+
   doctest ExChatwork
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup_all do
+    ExVCR.Config.filter_request_headers("X-ChatWorkToken")
+    ExVCR.Config.filter_sensitive_data("\"room_id\\\":[0-9]+", "\"room_id\":\"<REMOVED>\"")
+    ExVCR.Config.filter_sensitive_data("\"name\\\":\".+?\"", "\"name\":\"<REMOVED>\"")
+    ExVCR.Config.filter_sensitive_data("\"icon_path\\\":\".+?\"", "\"icon_path\":\"<REMOVED>\"")
+    HTTPoison.start
+    :ok
+  end
+
+  test "gets authenticated user's room list" do
+    use_cassette "get_rooms" do
+      response = ExChatwork.get_rooms
+      assert Enum.count(response.body) >= 1
+    end
   end
 end
